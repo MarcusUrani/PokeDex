15c3c3b712a2acc2da523d0e8d5f65a66a99c71d
Marcus Urani <marcus.urani20@gmail.com>
2022-02-21 12:27:06 -0300

    Adicionando a função de buscar pokemon por geração

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,37 +1,61 @@
-const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+const GEN_URL = "https://pokeapi.co/api/v2/generation/";
 
-//Função de buscar dados do pokémon
-const getData = () => {
-  const pokemonPromisses = [];
+const getPokemons = async (number) => {
+  const requisition = await fetch(`${BASE_URL}${number}/`);
+  const response = await requisition.json();
+  return response;
+};
 
-  //Loop para buscar dados de cada pokémon
-  for (let i = 1; i <= 898; i++) {
-    pokemonPromisses.push(
-      fetch(getPokemonUrl(i)).then((response) => response.json())
-    );
-  }
+const getPokemonsByGen = async (number) => {
+  const requisition = await fetch(`${GEN_URL}${number}`);
+  const response = await requisition.json();
+  const data = response.pokemon_species;
+  return data;
+};
 
+const getSpecies = async (url) => {
+  const requisition = await fetch(url);
+  const response = await requisition.json();
+  return response;
+};
+
+const constructHTML = (pokemonPromisses) => {
   //Criação do HTML
   Promise.all(pokemonPromisses).then((pokemons) => {
     const pokemonList = pokemons.reduce((accumulator, pokemon) => {
       accumulator += `<div class="main__card">
-           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-             pokemon.id
-           }.png"class="main__card__img"/>
+           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"class="main__card__img"/>
            <h4 class="main__card__title" id="pokemon-name">${pokemon.name}</h4>
            <p class="main__card__text">Peso: ${pokemon.weight}</p>
            <p class="main__card__text">Altura: ${pokemon.height}</p>
-          <p class="main__card__text">${pokemon.types
-            .map((typeInfo) => typeInfo.type.name)
-            .join(" | ")}</p>
          </div>`;
       return accumulator;
     }, "");
-
     //Inserção do HTML na página
     const pokeUl = document.querySelector(".main__cards");
     pokeUl.innerHTML = pokemonList;
   });
 };
 
-getData();
+//Função de buscar dados de todos os pokémons
+const getFirstData = () => {
+  const pokemonPromisses = [];
+  //Loop para buscar dados de cada pokémon
+  for (let i = 1; i <= 898; i++) {
+    pokemonPromisses.push(getPokemons(i));
+  }
+  constructHTML(pokemonPromisses);
+};
+
+const getGenData = async (genNumber) => {
+  const pokemonPromisses = [];
+  pokemonPromisses.splice((0)[898]);
+  const result = await getPokemonsByGen(genNumber);
+  result.map(async (pokemon) => {
+    console.log(await getSpecies(pokemon.url));
+  });
+  // constructHTML(pokemonPromisses);
+};
+
+getFirstData();
